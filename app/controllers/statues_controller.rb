e5a2e3ee448efb42303d8559d28163454029ef46
jesseborm <jesseborm@gmail.com>
2017-07-24 17:30:06 +0200

    Change name app, add Statue resource and statues_controller actions

---
+++ app/controllers/statues_controller.rb
@@ -0,0 +1,51 @@
+class StatuesController < ApplicationController
+  before :set_statue, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @statues = Statue.all
+  end
+
+  def create
+    @statue = Statue.new(statue_params)
+
+    if @statue.save
+       redirect_to @statue
+    else
+       render 'new'
+    end
+  end
+
+  def show
+  end
+
+  def new
+    @statue = Statue.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @statue.update_attributes(statue_params)
+      redirect_to @statue
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @statue.destroy
+
+    redirect_to statues_path
+  end
+
+private
+
+  def set_statue
+    @statue = Statue.find(params[:id])
+  end
+
+  def statue_params
+    params.require(:statue).permit(:name, :age, :image_url)
+  end
+end
