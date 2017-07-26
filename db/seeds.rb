b53bb9ebd195c98fba74f9ffc4a3e28c5d3f9fbf
jesseborm <jesseborm@gmail.com>
2017-07-26 11:37:32 +0200

    Add some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+statue1 = Statue.create!( { name: "Bunny", image_url: "https://s-media-cache-ak0.pinimg.com/originals/89/c9/27/89c9272d88f8c3cf9ca03bbf41a604a8.jpg" } )
+statue2 = Statue.create!( { name: "Horses",  image_url: "https://s-media-cache-ak0.pinimg.com/originals/59/36/45/593645747c4f26b008cdf21e520041cc.jpg" } )
+statue3 = Statue.create!( { name: "Elephant",  image_url: "http://schmulb.net/pics/pm/3-elephant.jpg" } )
+statue4 = Statue.create!( { name: "Owls", image_url: "https://s-media-cache-ak0.pinimg.com/736x/a4/e8/10/a4e810a6c91ca4d40d74708e48e70290--paper-mache-sculpture-art-sculpture.jpg" } )
