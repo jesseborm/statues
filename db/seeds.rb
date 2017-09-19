d6739bd55f763c329a0ebefd48977d509ed4220d
jesseborm <jesseborm@gmail.com>
2017-09-19 15:38:58 +0200

    extra seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,3 +4,6 @@ statue1 = Statue.create!( { name: "Bunny", image_url: "https://s-media-cache-ak0
 statue2 = Statue.create!( { name: "Horses",  image_url: "https://s-media-cache-ak0.pinimg.com/originals/59/36/45/593645747c4f26b008cdf21e520041cc.jpg" } )
 statue3 = Statue.create!( { name: "Elephant",  image_url: "http://schmulb.net/pics/pm/3-elephant.jpg" } )
 statue4 = Statue.create!( { name: "Owls", image_url: "https://s-media-cache-ak0.pinimg.com/736x/a4/e8/10/a4e810a6c91ca4d40d74708e48e70290--paper-mache-sculpture-art-sculpture.jpg" } )
+statue5 = Statue.create!( { name: "Rabbit", image_url: "https://i.pinimg.com/736x/7d/ab/f3/7dabf33704e968def20ac13e9945812f--paper-mache-sculpture-sculptures.jpg" } )
+statue6 = Statue.create!( { name: "Walrus", image_url: "https://s-media-cache-ak0.pinimg.com/originals/62/86/48/628648a0a635382b274b35d1efcede8e.jpg" } )
+statue7 = Statue.create!( { name: "Trea Head", image_url: "http://rdirenna.files.wordpress.com/2006/10/more001-2.JPG" } )
