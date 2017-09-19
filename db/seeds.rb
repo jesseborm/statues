98fa9c1fe8f7fd5b478acde34bcca071d6336e10
jesseborm <jesseborm@gmail.com>
2017-09-19 13:10:19 +0200

    Fix gemfile and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-=======
 Statue.destroy_all
 
->>>>>>> 88f236f25674d4c75f39a324544384c287141e76
 statue1 = Statue.create!( { name: "Bunny", image_url: "https://s-media-cache-ak0.pinimg.com/originals/89/c9/27/89c9272d88f8c3cf9ca03bbf41a604a8.jpg" } )
 statue2 = Statue.create!( { name: "Horses",  image_url: "https://s-media-cache-ak0.pinimg.com/originals/59/36/45/593645747c4f26b008cdf21e520041cc.jpg" } )
 statue3 = Statue.create!( { name: "Elephant",  image_url: "http://schmulb.net/pics/pm/3-elephant.jpg" } )
