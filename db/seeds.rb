1cdc1fe6d4724169879e09e5dc822373ebe017ee
jesseborm <jesseborm@gmail.com>
2017-10-13 16:37:56 +0200

    Add classes for css and flex-box

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,5 @@ statue3 = Statue.create!( { name: "Elephant",  image_url: "http://schmulb.net/pi
 statue4 = Statue.create!( { name: "Owls", image_url: "https://s-media-cache-ak0.pinimg.com/736x/a4/e8/10/a4e810a6c91ca4d40d74708e48e70290--paper-mache-sculpture-art-sculpture.jpg" } )
 statue5 = Statue.create!( { name: "Rabbit", image_url: "https://i.pinimg.com/736x/7d/ab/f3/7dabf33704e968def20ac13e9945812f--paper-mache-sculpture-sculptures.jpg" } )
 statue6 = Statue.create!( { name: "Walrus", image_url: "https://s-media-cache-ak0.pinimg.com/originals/62/86/48/628648a0a635382b274b35d1efcede8e.jpg" } )
+# statue6 = Statue.create!( { name: "Walrus", image_url: "../app/assets/images/walrus.jpg" } )
 statue7 = Statue.create!( { name: "Trea Head", image_url: "http://rdirenna.files.wordpress.com/2006/10/more001-2.JPG" } )
